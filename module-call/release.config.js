22fd0cd9556911ef7f3bd00196744ad807757dc8
Njoki88 <tabbz08@yahoo.com>
2023-09-23 23:57:05 -0500

    fix:semantic release Scope / Description with fix, feat (SemVer) Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET>cld-3 Problem Solution Note

---
+++ module-call/release.config.js
@@ -0,0 +1,8 @@
+export const branches = "main";
+export const repositoryurl = "https://github.com/NJOKI88/s3-backend-repo.git";
+export const Plugins = [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release/release-notes-generator,',
+    '@semantic-release/git',
+    '@semantic-release/github'
+];
