7168f89bd4bb10807d25308b3549d5140ffa8253
Njoki88 <tabbz08@yahoo.com>
2023-09-27 23:57:16 -0500

    fix: semantic release

--- module-call/release.config.js
+++ module-call/release.config.js
@@ -1,6 +1,6 @@
 module.exports = { 
     branches: "main", 
-    repositoryUrl: "https://github.com/Kenmakhanu/actionstest.git", 
+    repositoryUrl: "https://github.com/NJOKI88/s3-backend-repo.git", 
     plugins: [ 
       '@semantic-release/commit-analyzer', 
       '@semantic-release/release-notes-generator', 
