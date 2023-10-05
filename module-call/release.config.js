a6d21408c50b842ace9b472679d79ef82e05160a
Njoki88 <tabbz08@yahoo.com>
2023-10-05 00:27:12 -0500

    updates

--- module-call/release.config.js
+++ module-call/release.config.js
@@ -1,7 +1,8 @@
-module.exports = { 
-    branches: "main", 
-    repositoryUrl: "https://github.com/NJOKI88/s3-backend-repo.git", 
-    plugins: [ 
+  module.exports = {
+  branches: ["master", "next"],
+
+  repositoryUrl: "https://github.com/NJOKI88/s3-backend-repo.git", 
+  plugins: [ 
       '@semantic-release/commit-analyzer', 
       '@semantic-release/release-notes-generator', 
       '@semantic-release/git', 
