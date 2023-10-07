45f4d548c45ff86e1ce50636e64cfcdb73282ede
Njoki88 <tabbz08@yahoo.com>
2023-10-07 01:45:56 -0500

    updates

---
+++ s3- module/myfunc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source My functions.sh
+
+PS3="select your workspace [1-3] and press enter: "
+
+select env in dev stage uat
+do
+  case $env in
+    "dev")
+       environment;
+       workspace;
+       creds;
+       test;
+       break;;
+     "stage")
+         environment;
+         workspace;
+         creds;
+         apply;
+         break;;
+     "uat")
+       environment;
+       workspace;
+       test;
+       break;;
+    *)
+ esac
+ done
+    
